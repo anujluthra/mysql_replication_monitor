e9667488442aba6cd5e933ec34ed28145c1cf195
anujluthra <anuj.luthra@gmail.com>
2010-01-20 11:34:33 +1030

    now sms-emails are sent correctly.
    also checks that respective settings are present before sending email or sms

--- monitor.rb
+++ monitor.rb
@@ -5,7 +5,7 @@
 #
 # uses heartbeat timestamps to check the replication lag on server being monitored
 # and notifies when the lag in beyong the acceptable limit.
-# NOTE: 
+# NOTE:
 # 1. please make sure that master and slave server are in sync with same time server.
 #    eg. ntpdate ntp.ubuntu.com
 #
@@ -13,7 +13,7 @@
 #    at regular intervals. I suggest mk-heartbeat (see: http://www.maatkit.org/doc/mk-heartbeat.html)
 #
 # 3. library dependencies: mysql, net/smtp, time
-# 
+#
 # 4. Add it to cron job list to check the lag every {allowed_lag_in_seconds}+ interval
 #
 # It also attaches the last 100 lines of mysql error log to help you get an idea of whats going wrong.
@@ -25,10 +25,10 @@ require 'time'
 require 'net/smtp'
 require 'yaml'
 
-
 def send_email(message, from=@email_configs['sender'], to=@email_configs['recepients'])
+  formatted_message = @email_header + message
   Net::SMTP.start(@email_configs['smtp_host'], @email_configs['smtp_port']) do |smtp|
-    smtp.send_message message, from, to
+    smtp.send_message formatted_message, from, to
   end
 end
 
@@ -37,33 +37,11 @@ def send_sms(message,from=@sms_configs['sender'], to=@sms_configs['recepient'])
 end
 
 
-def email_message(latest_heartbeat, current_server_time, db_config)
-
-  error_log_last_100_lines = `tail -n20 #{db_config['error_log']}`
-  <<END_OF_MESSAGE
-  From: Replication Monitor
-  Subject: Replication lag on #{@server_name} for #{db_config['database']}
-
-  Last Heartbeat recieved via replication : #{latest_heartbeat}
-  The current time on replication server  : #{current_server_time}
-
-  Replication is lagging for more than the allowed limit of #{@allowed_lag} seconds.
-  Please check the replication server immidiately.
-
-  MYSQL ERROR OUTPUT (located at #{db_config['error_log']})
-  ======================================================
-  #{error_log_last_100_lines}
-
-END_OF_MESSAGE
-
-end
-
 def read_settings
-  configs = YAML::load(File.read 'config.yml')
+  configs = YAML::load(File.read('config.yml'))
   @heartbeat_table = configs['heartbeat_table']
   @allowed_lag = configs['allowed_lag'].to_i
   @server_name = configs['server_name']
-
   @email_configs = configs['email_configs']
   @sms_configs = configs['sms_configs']
   @db_configs = configs['databases']
@@ -73,14 +51,27 @@ def do_check(db_config)
   ################################################
   # Connect to database and fetch latest heartbeat
   db = Mysql.real_connect(db_config['host'], db_config['user'], db_config['password'], db_config['database'], db_config['port'])
-  latest_heartbeat = Time.parse(db.query("select ts from #{@heartbeat_table}").fetch_row.first)
+  query = "select ts from #{@heartbeat_table}"
+  latest_heartbeat = Time.parse(db.query(query).fetch_row.first)
   current_server_time = Time.now
 
   # Compare timestamps and notify if necessary
   if (current_server_time - @allowed_lag) > latest_heartbeat
-    send_email email_message(latest_heartbeat, current_server_time, db_config)
-    short_message = "fantasea replication is lagging on #{@server_name}"
-    send_sms short_message
+    puts 'lag detected...'
+
+    @email_header = gen_email_header(db_config)
+
+    if !@email_configs.nil?
+      puts 'sending email alert...'
+      send_email email_message(latest_heartbeat, current_server_time, db_config)
+    end
+
+    if !@sms_configs.nil?
+      puts 'sending sms alert...'
+      short_message = "fantasea replication is lagging on #{@server_name} by #{current_server_time - latest_heartbeat}seconds"
+      send_sms short_message
+    end
+
   end
 
 end
@@ -95,6 +86,34 @@ def start
 end
 
 
-start
+def gen_email_header(db_config)
+  <<-EOH
+From: Replication Monitor
+Subject: Replication lag on #{@server_name} for #{db_config['database']}
 
+  EOH
+end
+
+
+def email_message(latest_heartbeat, current_server_time, db_config)
+
+  error_log_last_100_lines = `tail -n20 #{db_config['error_log']}`
+  
+  <<END_OF_MESSAGE
+Last Heartbeat recieved via replication : #{latest_heartbeat}
+The current time on replication server  : #{current_server_time}
+
+Replication is lagging for more than the allowed limit of #{@allowed_lag} seconds.
+Please check the replication server immidiately.
+
+MYSQL ERROR OUTPUT (located at #{db_config['error_log']})
+======================================================
+#{error_log_last_100_lines}
+
+END_OF_MESSAGE
+
+end
+
+
+start
 
